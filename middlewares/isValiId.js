dded1ab7f876de5f6e3f71f68c483c90bb258949
Vladyslav Parkhomovych <adamantinevlad@gmail.com>
2023-12-24 10:13:14 +0200

    400

--- middlewares/isValiId.js
+++ middlewares/isValiId.js
@@ -3,7 +3,7 @@ import { HttpError } from "../helpers/index.js";
 
 const isValiId = (req, res, next) => {
   const { id } = req.params;
-  if (!isValidObjectId(id)) return next(HttpError(404, `${id} not valid id`));
+  if (!isValidObjectId(id)) return next(HttpError(400, `${id} not valid id`));
   next();
 };
 
